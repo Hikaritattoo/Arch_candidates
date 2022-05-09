92fe54ea357b5415b8d9362922ef29ab1cd867e1
Olga Smotrova <olgahikaritattoo@gmail.com>
2022-05-09 20:30:44 +0300

    initial commit

--- tests/vacancies_test.py
+++ tests/vacancies_test.py
@@ -7,6 +7,6 @@ class TestVacancies:
 
     def test_single_status(self, test_client):
         """ Проверяем, получается ли при запросе  вакансии нужный статус-код """
-        response = test_client.get('/vacancy/1', follow_redirects=True)
+        response = test_client.get('/vacancies/1', follow_redirects=True)
         assert response.status_code == 200, 'Статус код одной вакансии неверный'
 
