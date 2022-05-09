18b175c7f6dd195ef658c446798c8e5acae1388a
Olga Smotrova <olgahikaritattoo@gmail.com>
2022-05-09 19:12:57 +0300

    initial commit

--- app/candidates/views.py
+++ app/candidates/views.py
@@ -8,12 +8,16 @@ candidates_blueprint = Blueprint('candidates_blueprint', __name__, template_fold
 candidates_dao = CandidateDAO("./data/candidates.json")
 
 # Создаем вьюшку для кандидатов
+
+
 @candidates_blueprint.route('/candidates/')
 def page_candidates_all():
     candidates = candidates_dao.get_all()
     return render_template("candidates_index.html", candidates=candidates)
 
 # Создаем вьюшку для одного кандидата
+
+
 @candidates_blueprint.route('/candidates/<int:pk>/')
 def page_candidate_all(pk):
     candidate = candidates_dao.get_by_pk(pk)
--- app/vacancies/views.py
+++ app/vacancies/views.py
@@ -8,13 +8,13 @@ vacancies_blueprint = Blueprint('vacancies_blueprint', __name__, template_folder
 vacancies_dao = VacanciesDAO("./data/vacancies.json")
 
 
-@vacancies_blueprint.route('/vacancies')
+@vacancies_blueprint.route('/vacancies/')
 def page_vacancies():
     vacancies = vacancies_dao.get_all()
     return render_template("vacancies_index.html", vacancies=vacancies)
 
 
-@vacancies_blueprint.route('/vacancies/<int:pk>')
+@vacancies_blueprint.route('/vacancies/<int:pk>/')
 def page_vacancy(pk):
     vacancy = vacancies_dao.get_by_pk(1)
     return render_template("vacancies_single.html", vacancy=vacancy)
